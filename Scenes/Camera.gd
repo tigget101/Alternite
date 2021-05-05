44632d02e37d46a04667034f1ae523e4fd944397
tigget101 <82478392+tigget101@users.noreply.github.com>
2021-05-06 10:57:59 +1200

    Code + scenes

---
+++ Scenes/Camera.gd
@@ -0,0 +1,9 @@
+extends Camera
+
+
+
+onready var player_follow = get_parent().get_node("Player")
+
+func _process(delta):
+	var camera_pos = Vector3.ZERO
+	camera_pos.x = player_follow.translation.x
--- Scripts/Movement + Attack.gd
+++ Scripts/Movement + Attack.gd
@@ -1,57 +1,39 @@
 extends KinematicBody
 
-
-
 onready var camera = get_parent().get_node("Camera")
-export var max_speed = 10
+
+export var max_speed = 5
 export var friction = 10
 export var speed = 1
-export var acceleration = 0.5  
-export var mouse_sensitivity = 0.3
-
-
+export var acceleration = 0.5
 
 var move_vector = Vector3.ZERO
 var cursor_pos = Vector3.ZERO
-var can_fire = true
 
-func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	
-func _input(event):
-	if event is InputEventMouseMotion:
-		print(event.get_relative())
-		rotate_y(deg2rad(-event.get_relative().x) * mouse_sensitivity)
-
-#func look_at_cursor():
-#	var player_pos = global_transform.origin
-#	var drop_plane = Plane(Vector3(0,1,0), player_pos.y)
-#	var ray_length = 1000
-#	var mouse_pos = get_viewport().get_mouse_position()
-#	var from = camera.project_ray_origin(mouse_pos)
-#	var to = from + camera.project_ray_normal(mouse_pos) * ray_length
-#	cursor_pos = drop_plane.intersects_ray(from,to)
-#	#print(cursor_pos)
-#	look_at(cursor_pos, Vector3.UP)
-	
 func get_input():
 	var input = Vector3(
-		-int(Input.is_action_pressed("ui_left")) + int(Input.is_action_pressed("ui_right")),
-		0, 
-		-int(Input.is_action_pressed("ui_up")) + int(Input.is_action_pressed("ui_down"))
+		int(Input.is_action_pressed("ui_left")) -int(Input.is_action_pressed("ui_right")),
+		0,
+		0
 	)
 	input = input.normalized()
 	return input
+	
+func _process(delta):
+	pass
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	var input = get_input()
+	if input != Vector3.ZERO:
+		speed += acceleration
+		if speed > max_speed:
+			speed = max_speed
+			
+	move_vector = input * speed
+	move_and_slide(move_vector)
+			
+			
+			
+			
+			
+		
