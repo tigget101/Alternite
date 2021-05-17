5924932d2373e2551617c8f5eebd7f6a50955089
tigget101 <82478392+tigget101@users.noreply.github.com>
2021-05-17 12:25:10 +1200

    y

--- Scripts/Movement + Attack.gd
+++ Scripts/Movement + Attack.gd
@@ -4,57 +4,52 @@ onready var camera = get_parent().get_node("Camera")
 
 export var max_speed = 3.5
 export var friction = 10
-export var speed = 1
-export var acceleration = 0.5
-
-
-
+export var speed = 2.95
+export var acceleration = 10
 var move_vector = Vector3.ZERO
 var cursor_pos = Vector3.ZERO
-var gravity = 4
-var jump = 2.2
-var sproing = Vector3()
-var max_jump = 0
+var gravity = 6
+var jump = 3.5
+var fall = Vector3()
+var direction = Vector3()
+var velocity = Vector3()
+var jump_count = 0
 
-func get_input():
-	var input = Vector3(
-		int(Input.is_action_pressed("ui_left")) -int(Input.is_action_pressed("ui_right")),
-		0,
-		0
-	)
-	input = input.normalized()
-	return input
-	
 func _process(delta):
-	if is_on_floor():
-		max_jump = 0
-	if not is_on_floor():
-		sproing.y -= gravity * delta
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		if max_jump == 0:
-			sproing.y = jump
-			max_jump == 1
 	
-	if Input.is_action_just_pressed("jump") and not is_on_floor():
-		if max_jump == 1:
-			sproing.y = jump
-			max_jump = 2
-			
-	move_and_slide(sproing, Vector3.UP)
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		fall.y = jump
+		
+	if not is_on_floor():
+		fall.y -= gravity * delta
+		
+	direction = Vector3()
+	
+	if Input.is_action_pressed("ui_left"):
+		direction += transform.basis.z
+		
+	elif Input.is_action_pressed("ui_right"):
+		direction -= transform.basis.z
+		
+	direction = direction.normalized()
+	velocity = velocity.linear_interpolate(direction * speed, acceleration * delta)
+	velocity = move_and_slide(velocity, Vector3.UP)
+	move_and_slide(fall, Vector3.UP)
+
+		
+
+		
+	
 	
 
-func _physics_process(delta):
-	var input = get_input()
-	if input != Vector3.ZERO:
-		speed += acceleration
-		if speed > max_speed:
-			speed = max_speed
-			
-	move_vector = input * speed
-	move_and_slide(move_vector)
+
+func _ready():
+	pass
+
+
 			
 			
-			
+
 			
 			
 		
