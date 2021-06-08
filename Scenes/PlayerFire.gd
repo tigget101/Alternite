211b9be9db09216b1e305456ab4d9cbc7617a52d
tigget101 <82478392+tigget101@users.noreply.github.com>
2021-06-09 09:57:12 +1200

    playerfire

---
+++ Scenes/PlayerFire.gd
@@ -0,0 +1,16 @@
+extends RigidBody
+
+
+export var bullet_speed = 5000
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	add_to_group("ignore")
+	pass
+	 # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
