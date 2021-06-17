40cfe34c82d37802f701ff3ccbb4014cc3dc5465
tigget101 <82478392+tigget101@users.noreply.github.com>
2021-06-18 10:55:50 +1200

    UI_cont

--- Scripts/Player_Stats.gd
+++ Scripts/Player_Stats.gd
@@ -14,3 +14,27 @@ func _ready():
 	max_light = 100
 	lives = 3
 	max_lives = 5
+	
+func change_life(amount):
+	life += amount
+	life = clamp(life,0, max_life)
+	
+func change_light(amount):
+	light += amount
+	light = clamp(light,0, max_light)
+
+func change_lives(amount):
+	lives += amount
+	lives = clamp(lives,0, max_lives)
+	
+func get_life():
+	return life
+	
+func get_light():
+	return light
+	
+func get_lives():
+	return lives
+	
+func has_ammo():
+	return light > 0
